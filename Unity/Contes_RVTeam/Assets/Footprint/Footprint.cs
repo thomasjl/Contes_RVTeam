c6f8cb8666aadd7e172bb7f87dfac9538e0efbad
thomasjl <thomjl78@gmail.com>
2019-03-07 21:02:33 +0100

    Footprint Done

---
+++ Unity/Contes_RVTeam/Assets/Footprint/Footprint.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footprint : MonoBehaviour
+{
+    public Material footPrintClosed;
+    public Material footPrintFar;
+    public Material outOfRangeFootprint;
+
+    private GameObject lanterne;
+    private float maxDist;
+    private Renderer rend;
+
+    private void Start()
+    {
+        lanterne = GameObject.FindGameObjectWithTag("Lanterne");
+        maxDist = lanterne.GetComponent<SphereCollider>().radius;
+        rend = GetComponent<Renderer>();
+
+        rend.material = outOfRangeFootprint;
+    }
+        
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag =="Lanterne")
+        {
+            // set transparacy depending on distance from Lanterne (max distance = radius)
+            float distance = Vector3.Distance(transform.position, other.gameObject.transform.position);
+           // Debug.Log("distance " +distance);
+            float ratio = distance / maxDist;
+            //Debug.Log("ratio "+ratio);
+            //lerp material
+            rend.material.Lerp(footPrintClosed,footPrintFar,ratio);
+
+
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Lanterne")
+        {
+
+            rend.material = outOfRangeFootprint;
+        }
+    }
+
+   
+}
---
+++ Unity/Contes_RVTeam/Assets/Footprint/test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class test : MonoBehaviour {
+
+    void OnEnable()
+    {
+        Debug.Log("Testing connection for devices");
+        SteamVR_Events.DeviceConnected.Listen(OnDeviceConnected);
+    }
+
+    // A SteamVR device got connected/disconnected
+    private void OnDeviceConnected(int index, bool connected)
+    {
+
+        if (connected)
+        {
+            if (OpenVR.System != null)
+            {
+                //lets figure what type of device got connected
+                ETrackedDeviceClass deviceClass = OpenVR.System.GetTrackedDeviceClass((uint)index);
+                if (deviceClass == ETrackedDeviceClass.Controller)
+                {
+                    Debug.Log("Controller got connected at index:" + index);
+                }
+                else if (deviceClass == ETrackedDeviceClass.GenericTracker)
+                {
+                    Debug.Log("Tracker got connected at index:" + index);
+                }
+            }
+        }
+    }
+}
