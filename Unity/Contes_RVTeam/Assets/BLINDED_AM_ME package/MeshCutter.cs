cceb3b0eb9cc6e97f038f983661625f54c087a13
Simon Pasi <simon.pasi@ensam.eu>
2019-03-08 19:08:15 +0100

    Début tests cut buches

---
+++ Unity/Contes_RVTeam/Assets/BLINDED_AM_ME package/MeshCutter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MeshCutter : MonoBehaviour {
+
+    [SerializeField]
+    Material capMaterial;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                GameObject victim = hit.collider.gameObject;
+                GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+            }
+        }
+    }
+}
---
+++ Unity/Contes_RVTeam/Assets/Props/Chaperon Rouge/Buches/Buche.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Buche : MonoBehaviour {
+
+    [SerializeField]
+    Material capMaterial;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Hache hache = other.GetComponent<Hache>();
+        if (hache && hache.IsSlicing)
+            Cut(hache);
+    }
+
+    void Cut(Hache hache)
+    {
+        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(transform.GetChild(0).gameObject, transform.position, hache.SliceDirection, capMaterial);
+    }
+}
