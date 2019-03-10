e3f224335051d93b0a165eb7f2bf1a8f8d3e0913
Simon Pasi <simon.pasi@ensam.eu>
2019-03-10 12:05:54 +0100

    Flamme lanterne

--- Unity/Contes_RVTeam/Assets/Props/Chaperon Rouge/Medaillon/Medaillon.cs
+++ Unity/Contes_RVTeam/Assets/Props/Chaperon Rouge/Medaillon/Medaillon.cs
@@ -9,30 +9,53 @@ public class Medaillon : MonoBehaviour {
     [SerializeField]
     float distanceFromThorns = 1;
 
+    [SerializeField]
+    float minLanterneDistance = .2f;
+    [SerializeField]
+    float maxLanterneDistance = 1;
+    [SerializeField]
+    float minAudioVolume = .2f;
+    new AudioSource audio;
+
     private void Awake()
     {
         interactable = GetComponent<Interactable>();
         interactable.onAttachedToHand += OnGrab;
         interactable.onDetachedFromHand += OnUngrab;
-    }
-
-    private void OnUngrab(Hand hand)
-    {
-        throw new NotImplementedException();
+        audio = GetComponent<AudioSource>();
     }
 
     private void OnGrab(Hand hand)
     {
-        throw new NotImplementedException();
+        StartCoroutine(UpdateThorns());
+        StartCoroutine(UpdateSound());
     }
 
-    IEnumerator UpdateThorns(){
-        while (true)
+    private void OnUngrab(Hand hand)
+    {
+        StopAllCoroutines();
+        audio.Stop();
+    }
+
+    IEnumerator UpdateThorns()
+    {
+        while (Thorns.instance)
         {
             if (Vector3.Distance(transform.position, Thorns.instance.transform.position) < distanceFromThorns)
                 Thorns.instance.Flatten();
             else
                 Thorns.instance.Unflatten();
+            yield return null;
+        }
+    }
+
+    IEnumerator UpdateSound()
+    {
+        audio.Play();
+        while (true)
+        {
+            float progression = 1 - (Vector3.Distance(transform.position, Lanterne.instance.transform.position) / (maxLanterneDistance - minLanterneDistance));
+            audio.volume = Mathf.Lerp(minAudioVolume, 1, progression);
             yield return null;
         }
     }
