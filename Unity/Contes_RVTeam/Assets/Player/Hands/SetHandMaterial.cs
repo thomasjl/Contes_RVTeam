6a94219c7ed5c5c493f1012d46c42527f1c6ffa1
Simon Pasi <simon.pasi@ensam.eu>
2019-03-11 17:00:11 +0100

    Couleur des mains

---
+++ Unity/Contes_RVTeam/Assets/Player/Hands/SetHandMaterial.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class SetHandMaterial : MonoBehaviour {
+
+    [SerializeField]
+    Material mat;
+    private void Start()
+    {
+        GetComponent<RenderModel>().SetHandMaterial(mat);
+    }
+}
