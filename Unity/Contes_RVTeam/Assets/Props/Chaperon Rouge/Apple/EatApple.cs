e803453669306b69e49df933d3a2829e55862853
Oxicrom <oxycrom@gmail.com>
2019-03-11 10:59:53 +0100

    Integration props

---
+++ Unity/Contes_RVTeam/Assets/Props/Chaperon Rouge/Apple/EatApple.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatApple : MonoBehaviour
+{
+
+    public GameObject eatenApple;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "HeadCollider")
+        {
+            GetComponent<MeshFilter>().mesh = eatenApple.GetComponent<MeshFilter>().sharedMesh;
+            GetComponent<MeshRenderer>().material = eatenApple.GetComponent<MeshRenderer>().sharedMaterial;
+        }
+    }
+
+    
+}
