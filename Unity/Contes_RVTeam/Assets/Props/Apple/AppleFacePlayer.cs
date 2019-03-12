23a70c699c2da2573fa0ca6812b6fd700f7cdc31
Simon Pasi <simon.pasi@ensam.eu>
2019-03-12 12:39:34 +0100

    Rotation de la pomme

---
+++ Unity/Contes_RVTeam/Assets/Props/Apple/AppleFacePlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class AppleFacePlayer : MonoBehaviour {
+    Comestible comestible;
+    private void Awake()
+    {
+        comestible = GetComponentInParent<Comestible>();
+        comestible.Consumed += FacePlayer;
+    }
+
+    private void FacePlayer()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.parent.up, Player.instance.headCollider.transform.position - transform.position);
+    }
+}
