f32107b4f417a764618ed81aec718eb4452bf463
Chaliu <cha.dubosc@gmail.com@gmail.com>
2019-03-13 15:10:05 +0100

    fbx thorns

---
+++ Unity/Contes_RVTeam/Assets/Props/Alice/Queen paiting board/Table.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Table : MonoBehaviour {
+
+    [SerializeField]
+    Transform tableObjects;
+    [SerializeField]
+    Potion potionTemplate;
+    [SerializeField]
+    Transform potionSpawn;
+
+    public static Table Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void AddPotion()
+    {
+        tableObjects.DestroyChidren();
+        Instantiate(potionTemplate, potionSpawn);
+    }
+}
