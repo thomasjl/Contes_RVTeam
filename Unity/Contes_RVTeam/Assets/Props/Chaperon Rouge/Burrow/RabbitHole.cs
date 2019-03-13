c5a184139a50caae0804939738275f0fea888198
Simon Pasi <simon.pasi@ensam.eu>
2019-03-13 15:26:30 +0100

    Début intégration prefabs scène 2

---
+++ Unity/Contes_RVTeam/Assets/Props/Chaperon Rouge/Burrow/RabbitHole.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RabbitHole : MonoBehaviour {
+
+    [SerializeField]
+    SceneAsset nextScene;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HeadCollider") && (!Thorns.instance || Thorns.instance.Cleared))
+            ToNextScene();
+    }
+
+    void ToNextScene()
+    {
+        PlayerPostProcess.Instance.PlayBlinkFadeOut(1, delegate { this.Timer(2, delegate { SceneManager.LoadSceneAsync(nextScene.name); }); });
+    }
+}
