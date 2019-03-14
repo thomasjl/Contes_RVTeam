fe188cd0200b565e9b1ebd7b0e97650aa418061b
Simon Pasi <simon.pasi@ensam.eu>
2019-03-14 19:09:11 +0100

    Correction corde du puits

---
+++ Unity/Contes_RVTeam/Assets/Props/Blanche Neige en attente de merge/Lift.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Lift : MonoBehaviour {
+
+[SerializeField]
+    CircularDrive circularDrive;
+
+    float previousDrive;
+
+    private void Update()
+    {
+        previousDrive = circularDrive.outAngle;
+    }
+}
