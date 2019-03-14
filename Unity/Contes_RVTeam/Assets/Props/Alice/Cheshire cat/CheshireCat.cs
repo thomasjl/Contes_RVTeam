53e42d27541fa0f06dc8b378fdbe0ddf0cefb042
Simon Pasi <simon.pasi@ensam.eu>
2019-03-14 14:43:26 +0100

    Chat + révision miroir

--- Unity/Contes_RVTeam/Assets/Props/Alice/Cheshire cat/CheshireCat.cs
+++ Unity/Contes_RVTeam/Assets/Props/Alice/Cheshire cat/CheshireCat.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
 using Valve.VR.InteractionSystem;
 
 public class CheshireCat : MonoBehaviour {
@@ -25,10 +28,21 @@ public class CheshireCat : MonoBehaviour {
     [SerializeField]
     float playerDistance = 2;
     [SerializeField]
-    new AudioSource audio;
-    bool talked = false;
+    List<VideoClip> startVideos = new List<VideoClip>();
+    [SerializeField]
+    VideoClip[] randomVideos;
 
-    Vector3 Target { get { return Player.instance.transform.position; } }
+    VideoPlayer playerA;
+    VideoPlayer playerB;
+    bool useA = true;
+    int lastVideo = -1;
+    bool aPlays;
+    bool bPlays;
+    bool aPrepared;
+    bool bPrepared;
+
+    Vector3 Target { get { return Player.instance.headCollider.transform.position; } }
+
 
     private void Awake()
     {
@@ -36,11 +50,35 @@ public class CheshireCat : MonoBehaviour {
         leftMaxX = cat.InverseTransformPoint(leftLimits.bounds.max).x;
         rightMinX = cat.InverseTransformPoint(rightLimits.bounds.min).x;
         rightMaxX = cat.InverseTransformPoint(rightLimits.bounds.max).x;
+
+        // Setup video players.
+        playerA = GetComponentsInChildren<VideoPlayer>()[0];
+        playerB = GetComponentsInChildren<VideoPlayer>()[1];
+        playerA.loopPointReached += delegate { aPlays = false; };
+        playerA.prepareCompleted += delegate
+        {
+            aPrepared = true;
+        };
+        playerB.loopPointReached += delegate { bPlays = false; };
+        playerB.prepareCompleted += delegate
+        {
+            bPrepared = true;
+        };
     }
 
     private void Start()
     {
         Teleport();
+
+        // Start the video at the first frame.
+        playerA.clip = startVideos[0];
+        startVideos.RemoveAt(0);
+        playerA.Play();
+        playerA.Pause();
+        playerA.frame = 1;
+        playerA.Prepare();
+
+        Potion.ScaledNormal += Talk;
     }
 
     public void Teleport()
@@ -51,15 +89,19 @@ public class CheshireCat : MonoBehaviour {
 
     private void Update()
     {
-        LookAt(Player.instance.transform.position);
-        if (!talked && Vector3.Distance(Target, cat.position) > playerDistance)
-            Talk();
+        LookAt(Player.instance.headCollider.transform.position);
+        if (startVideos.Count > 1 && Vector3.Distance(Target, cat.position) < playerDistance)
+            SayStartSentence();
+    }
+
+    void SayStartSentence()
+    {
+        StartCoroutine(PlayStartVideos());
     }
 
     void Talk()
     {
-        audio.Play();
-        talked = true;
+        StartCoroutine(PlayRandomVideo());
     }
 
     void LookAt(Vector3 target)
@@ -70,10 +112,79 @@ public class CheshireCat : MonoBehaviour {
         float dotLeft = Vector3.Dot((target - leftLimits.transform.position).normalized, cat.right);
         float dotRight = Vector3.Dot((target - rightLimits.transform.position).normalized, cat.right);
 
-        float newLeftX = Mathf.Lerp(leftMinX, leftMaxX, (dotLeft + 1) * .5f);
-        float newRightX = Mathf.Lerp(rightMinX, rightMaxX, (dotRight + 1) * .5f);
+        float newLeftX = Mathf.Lerp(leftMinX, leftMaxX, 1 - ((dotLeft + 1) * .5f));
+        float newRightX = Mathf.Lerp(rightMinX, rightMaxX, 1 - ((dotRight + 1) * .5f));
 
         leftEye.localPosition = leftEye.localPosition.SetX(newLeftX);
         rightEye.localPosition = rightEye.localPosition.SetX(newRightX);
+    }
+
+    IEnumerator PlayStartVideos()
+    {
+        PlayPreparedClip();
+        PrepareClip(startVideos[0]);
+        startVideos.RemoveAt(0);
+        while (aPlays || bPlays || !aPrepared || !bPrepared)
+            yield return null;
+        yield return new WaitForSeconds(1);
+
+        PlayPreparedClip();
+        PrepareClip(startVideos[0]);
+        startVideos.RemoveAt(0);
+        while (aPlays || bPlays || !aPrepared || !bPrepared)
+            yield return null;
+        yield return new WaitForSeconds(1);
+
+        PlayPreparedClip();
+        while (aPlays || bPlays)
+            yield return null;
+
+        lastVideo = Utilities.ExclusiveRange(0, randomVideos.Length, lastVideo);
+        PrepareClip(randomVideos[lastVideo]);
+    }
+
+    IEnumerator PlayRandomVideo()
+    {
+        PlayPreparedClip();
+
+        lastVideo = Utilities.ExclusiveRange(0, randomVideos.Length, lastVideo);
+        PrepareClip(randomVideos[lastVideo]);
+        while (aPlays || bPlays || !aPrepared || !bPrepared)
+            yield return null;
+    }
+
+    void PrepareClip(VideoClip clip)
+    {
+        if (!useA)
+        {
+            bPrepared = false;
+            playerB.clip = clip;
+            playerB.Prepare();
+        }
+        else
+        {
+            aPrepared = false;
+            playerA.clip = clip;
+            playerA.Prepare();
+        }
+    }
+
+    void PlayPreparedClip()
+    {
+        if (useA)
+        {
+            playerA.targetMaterialRenderer.gameObject.SetActive(true);
+            playerA.Play();
+            aPlays = true;
+            playerB.targetMaterialRenderer.gameObject.SetActive(false);
+        }
+        else
+        {
+            playerB.targetMaterialRenderer.gameObject.SetActive(true);
+            playerB.Play();
+            bPlays = true;
+            playerA.targetMaterialRenderer.gameObject.SetActive(false);
+        }
+        useA = !useA;
     }
 }
