0ae3103755ba47f850f5b146b33064210fb28653
Simon Pasi <s.powercord@gmail.com>
2019-03-18 14:35:10 +0100

    Fin du commit

--- Unity/Contes_RVTeam/Assets/Props/Alice/Mushrooms/GoodMushroom.cs
+++ Unity/Contes_RVTeam/Assets/Props/Alice/Mushrooms/GoodMushroom.cs
@@ -8,33 +8,72 @@ public class GoodMushroom : Mushroom {
     float newPlayerSize = 3;
 
     public static List<GoodMushroom> mushrooms = new List<GoodMushroom>();
-    float transitionTime = 2.5f;
 
+<<<<<<< HEAD
     Vector3 startPosition;
     Quaternion startRotation;
+=======
+>>>>>>> 7f2ed639bcef553ee88eace6d1728b9f247cbdef
 
     protected override void Awake()
     {
         base.Awake();
         mushrooms.Add(this);
+<<<<<<< HEAD
         startPosition = transform.position;
         startRotation = transform.rotation;
         // Create player scale manager.
         if (!PlayerScaleManager.Instance)
             new GameObject().AddComponent<PlayerScaleManager>();
+=======
+>>>>>>> 7f2ed639bcef553ee88eace6d1728b9f247cbdef
     }
 
     protected override void OnConsumed()
     {
+<<<<<<< HEAD
         PlayerScaleManager.Instance.TryScaleDown(newPlayerSize, duration);
 
+=======
+        Vector3 startPosition = Player.instance.trackingOriginTransform.position;
+        Vector3 targetPosition = (Player.instance.headCollider.transform.position - (Player.instance.headCollider.transform.position * sizeFactor)).SetY(0);
+        Player.instance.ProgressionAnim(2, delegate (float progression)
+        {
+            // Animate in.
+            Player.instance.transform.position = Vector3.Lerp(startPosition, targetPosition, progression);
+            Player.instance.transform.localScale = Mathf.Lerp(1, sizeFactor, progression) * Vector3.one;
+        }, delegate
+        {
+            Lanterne.instance.PlayColorAnim(duration, Color.white);
+            Player.instance.Timer(duration, delegate
+            {
+                Player.instance.ProgressionAnim(2, delegate (float progression)
+                {
+                    // Animate out.
+                    Player.instance.transform.localScale = Mathf.Lerp(sizeFactor, 1, progression) * Vector3.one;
+                    Player.instance.headCollider.transform.localScale = Vector3.one / Player.instance.transform.localScale.x;
+                    Player.instance.transform.position = Vector3.Lerp(targetPosition, startPosition, progression);
+
+                }, delegate
+                {/*
+                    if (Crown.Instance.IsEquipped || Scepter.Instance.IsEquipped)
+                        CheshireCat.Instance.Spawn();*/
+                    Table.Instance.AddPotion();
+                });
+            });
+        });
+>>>>>>> 7f2ed639bcef553ee88eace6d1728b9f247cbdef
         // Create another mushroom.
         if (mushrooms.Count <= 1)
         {
             mushrooms.Remove(this);
             GameObject newMush = Instantiate(gameObject);
+<<<<<<< HEAD
             newMush.transform.position = startPosition;
             newMush.transform.rotation = startRotation;
+=======
+            newMush.transform.position = Vector3.up * .3f;
+>>>>>>> 7f2ed639bcef553ee88eace6d1728b9f247cbdef
             newMush.SetActive(true);
         }
     }
--- Unity/Contes_RVTeam/Assets/Props/Alice/Potion/Potion.cs
+++ Unity/Contes_RVTeam/Assets/Props/Alice/Potion/Potion.cs
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 ﻿using UnityEngine;
+=======
+﻿using System.Collections;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+>>>>>>> 7f2ed639bcef553ee88eace6d1728b9f247cbdef
 
 [RequireComponent(typeof(Comestible))]
 public class Potion : MonoBehaviour {
@@ -6,6 +12,7 @@ public class Potion : MonoBehaviour {
     [SerializeField]
     float newPlayerSize = .3f;
     [SerializeField]
+<<<<<<< HEAD
     float duration = 15;
     float transitionTime = 2.5f;
 
@@ -15,13 +22,68 @@ public class Potion : MonoBehaviour {
         GetComponent<Comestible>().Consumed += OnConsumed;
         if (!PlayerScaleManager.Instance)
             new GameObject().AddComponent<PlayerScaleManager>();
+=======
+    float timer = 10;
+
+    Vector3 ScaleCompensatedPosition { get { return (Player.instance.headCollider.transform.position - (Player.instance.headCollider.transform.position * newPlayerSize)).SetY(0); } }
+
+    public delegate void EventHandler();
+    public static event EventHandler ScaledNormal;
+    public static event EventHandler ScaledDown;
+
+    private void Awake()
+    {
+        GetComponent<Comestible>().Consumed += PlayEffect;
+>>>>>>> 7f2ed639bcef553ee88eace6d1728b9f247cbdef
     }
 
-    public void OnConsumed()
+    public void PlayEffect()
     {
+<<<<<<< HEAD
         PlayerScaleManager.Instance.TryScaleDown(newPlayerSize, duration);
 
         // Create another potion.
+=======
+        Vector3 startPosition = Player.instance.transform.position;
+        Player.instance.ProgressionAnim(2, delegate (float progression)
+        {
+            // Animate in.
+            Player.instance.transform.position = Vector3.Lerp(startPosition, SmallDoor.instance.Spawnpoint.position, progression);
+            Player.instance.transform.localScale = Mathf.Lerp(1, newPlayerSize, progression) * Vector3.one;
+
+        }, delegate
+        {
+            // Respawn a bottle.
+            Table.Instance.AddPotion();
+            // Call event.
+            if (ScaledDown != null)
+                ScaledDown();
+            Lanterne.instance.PlayColorAnim(timer, Color.white);
+            Player.instance.Timer(timer, delegate
+            {
+                Player.instance.ProgressionAnim(2, delegate (float progression)
+                {
+                    Player.instance.transform.localScale = Mathf.Lerp(newPlayerSize, 1, progression) * Vector3.one;
+                    Player.instance.transform.position = Vector3.Lerp(SmallDoor.instance.Spawnpoint.position, startPosition, progression);
+                }, delegate
+                {
+                    // Call event.
+                    if (ScaledNormal != null)
+                        ScaledNormal();
+                });
+            });
+        });
+
+
+
+
+        StartCoroutine(SpawnNewPotion());
+    }
+
+    IEnumerator SpawnNewPotion()
+    {
+        yield return new WaitForSeconds(10);
+>>>>>>> 7f2ed639bcef553ee88eace6d1728b9f247cbdef
         Table.Instance.AddPotion();
     }
 }
