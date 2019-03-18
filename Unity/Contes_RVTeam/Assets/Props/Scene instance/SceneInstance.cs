7b655d9e3f4c190c5cb6fe5ab24da8b2492fdb77
thomasjl <thomjl78@gmail.com>
2019-03-18 16:39:47 +0100

    changes in room3, instancescene

---
+++ Unity/Contes_RVTeam/Assets/Props/Scene instance/SceneInstance.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneInstance : MonoBehaviour {
+
+    private static SceneInstance instance;
+
+    private void Awake()
+    {
+        if(instance==null)
+        {
+            instance = this;
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+
+        }
+        else
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+}
